2451950756e8a31b75156d16dd5c6e28f7ff8ab4
Tarun Sharma <shar07107@gmail.com>
2024-10-01 01:43:23 +0530

    UI Part Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import "./App.css";
-// import Router from "./Router";
+import Router from "./Router";
 
 function App() {
 	return (
 		<main className="bg-white">
-			<BrowserRouter>{/* <Router /> */}</BrowserRouter>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
 		</main>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// import "./assets/css/style.scss";
+import "./assets/css/style.scss";
 import App from "./App.jsx";
 import "./index.css";
 
